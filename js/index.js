2e3542be158797a6161abc54769618dd4583f97e
Nilton César <104584538+NiltonCsantos@users.noreply.github.com>
2023-01-24 09:27:18 -0300

    Atualização

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  
+
   const typed = new Typed(".title-cover-content", {
       strings:["A melhor feijoada do Estado", "Os melhores preços", "Tradição e qualidade"],
-      typeSpeed:100,
+      typeSpeed:80,
       backSpeed:60,
       loop:true
   });
