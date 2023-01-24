eeccadd02110f59fec88ab18889db34ac107789b
Nilton César <104584538+NiltonCsantos@users.noreply.github.com>
2023-01-24 12:01:32 -0300

    Menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   const typed = new Typed(".title-cover-content", {
-      strings:["A melhor feijoada do Estado", "Os melhores preços", "Tradição e qualidade"],
+      strings:["A melhor feijoada do Estado!", "Os melhores preços!", "Tradição e qualidade!"],
       typeSpeed:80,
       backSpeed:60,
       loop:true
